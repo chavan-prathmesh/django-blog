fe4b918368d01b1c034724a7dc12cee6859df83c
chavan-prathmesh <prathmeshchavan517@gmail.com>
2025-12-11 18:47:36 +0530

    blogs at search feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from assignments.models import About
 from blogs.models import Blog, Category
 
 
@@ -7,8 +8,14 @@ def home(request):
     featured_post = Blog.objects.filter(is_featured=True,status='Published')
     posts = Blog.objects.filter(is_featured=False,status='Published')
 
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+
     context = {
         'featured_post':featured_post,
         'posts':posts,
+        'about':about,
     }
     return render(request,'home.html',context)
